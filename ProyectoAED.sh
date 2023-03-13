c35f25279fae671efe3390184dc1cf9ac9409a04
Diego Martínez <a353198@uach.mx>
2023-03-13 10:46:24 -0600

    Se modificó el menú principal

--- ProyectoAED.sh
+++ ProyectoAED.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
-
-echo "-a | Metodologías ágiles"
-echo "-t | Metodologías tradicionales"
-echo ""
+if [ $# != 1 ]; then
+	echo "Uso: $0 [ -a | -t ]"
+  echo "-a | Metodologías ágiles"
+  echo "-t | Metodologías tradicionales"
+	exit 1
+fi
 
 if [ "$1" = "-a" ]; then
-  echo "Metodologías ágiles"
-  echo "Bienvenido a la guía rápida de Agile, para continuar seleccione un tema:"
-  echo "1. SCRUM"
-  echo "2. X.P."
-  echo "3. Kanban"
-  echo "4. Crystal"
+	echo "\nMetodologías ágiles"
+	echo "Bienvenido a la guía rápida de Agile, para continuar seleccione un tema:"
+	echo "1. SCRUM"
+	echo "2. X.P."
+	echo "3. Kanban"
+	echo "4. Crystal\n"
+
 elif [ "$1" = "-t" ]; then
-  echo "Metodologías tradicionales"
-  echo "Bienvenido a la guía rápida de metodologías tradicionales, para continuar seleccione un tema:"
-  echo "1. Cascada"
-  echo "2. Espiral"
-  echo "3. Modelo V"
+	echo "\nMetodologías tradicionales"
+	echo "Bienvenido a la guía rápida de metodologías tradicionales, para continuar seleccione un tema:"
+	echo "1. Cascada"
+	echo "2. Espiral"
+	echo "3. Modelo V\n"
+	
 else
-  echo "Parámetro inválido. Debe usar -a para mostrar un texto o -t para mostrar otro texto"
-  exit 1
+	echo "Parámetro inválido. Debe usar -a para mostrar un texto o -t para mostrar otro texto"
+	exit 1
 fi
